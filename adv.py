4f8e9c36cb2e4966dd49fe64bc9ba806e827c156
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2019-12-10 18:09:23 -0500

    Update adv.py
    A li'l more :)

--- adv.py
+++ adv.py
@@ -1,5 +1,5 @@
-# I am coding blindly (using a Chromebook at school) so it may not work
-
+# I am coding blindly (#lazy) so it may not work
+crowbar = True
 def room1():
     print("You are in a room, with a Python Software Foundation poster. There is no escape other than a small barred window.")
     if crowbarTaken not True:
@@ -10,10 +10,13 @@ room1()
 whatToDo = input(">")
 if whatToDo == "take crowbar":
     if crowbarExists == True:
-    print("Taken.")
-    crowbarTaken = True
+        print("Taken.")
+        crowbarTaken = True
+    else:
+        print("I don't see that.")
 elif whatToDo == "take":
     print("You must supply an object.")
+elif "take" in whatToDo and whatToDo != "take": #It SHOULD in theroy see if it is `take' with another word (if there is take but the input is not exactly take
 elif whatToDo == "pull window":
     if windowExists == True:
         print("You cannot pull that.")
