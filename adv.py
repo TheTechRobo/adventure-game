6b26d9df932cdf7fc23ba4b96c051dae6c2f43c3
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-12-14 21:10:53 -0500

    .

--- adv.py
+++ adv.py
@@ -1,22 +1,31 @@
 # I am coding blindly (#lazy) so it may not work
 crowbar = True
 def room1():
-    print("You are in a room, with a Python Software Foundation poster. There is no escape other than a small barred window.")
-    if crowbarTaken not True:
+    print("You are in a room, with a Python Software Foundation poster. There is no visible escape other than a small barred window.")
+    if crowbarExists == True:
         print("There is a crowbar.")
     windowExists = True
     crowbarExists = True
 room1()
 whatToDo = input(">")
-if whatToDo == "take crowbar":
+if whatToDo.lower() in "take crowbar":
     if crowbarExists == True:
         print("Taken.")
         crowbarTaken = True
+        crowbarExists = False
+    elif crowbarExists == False:
+        print("You have already taken that!")
     else:
-        print("I don't see that.")
-elif whatToDo == "take":
+        print("I don't see that here.")
+elif whatToDo.lower() in "take":
     print("You must supply an object.")
 elif "take" in whatToDo and whatToDo != "take": #It SHOULD in theroy see if it is `take' with another word (if there is take but the input is not exactly take
-elif whatToDo == "pull window":
+    print("I don't see that here...")
+elif whatToDo.lower() in "pull window":
     if windowExists == True:
         print("You cannot pull that.")
+elif whatToDo.lower() in "pull bars":
+        print("You cannot pull that. Maybe if you had a tool...")
+elif whatToDo.lower() in "yeet crowbar":
+    if crowbarExists == True:
+        print("You throw the crowbar across the room. But it just bounces back.")
