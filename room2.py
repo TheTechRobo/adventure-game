529127f67bf1f28a7fcb25720adfc6f6cea92bd2
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-17 18:00:17 -0400

    Add a paper

--- room2.py
+++ room2.py
@@ -1,1 +1,9 @@
 print("You are in a new room. There is a piece of paper here.")
+def readPaper():
+  print('''The paper reads:
+  "Hello Manakivo, 
+  
+  I know that I will be long gone before you read this but I just wanted to tell you that "
+  The ink is smudged here. After the smudge, it reads:
+  "I hope that this letter finds you. I cannot escape. I cannot escape."
+  ''')
